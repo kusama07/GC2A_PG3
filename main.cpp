0ae7a5f629c50662c46326b4c2186b6ec5d9bc80
kusama07 <115055012+kusama07@users.noreply.github.com>
2025-04-11 14:33:46 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
+template <typename Type>
+Type Min(Type a, Type b) {
+	return a < b ? a : b;
+}
+
+template <>
+char Min<char>(char a, char b) {
+	return printf("数字以外は代入できません");
+}
+
 int main() {
 
-	char str[] = "あいえお";
-	printf("%s", str);
+	printf("%d\n", Min<int>(1, 2));
+	printf("%.1f\n", Min<float>(2.5f, 1.5f));
+	printf("%lf\n", Min<double>(7.55425, 5.23625));
+	printf("%c\n", Min<char>('a', 'b'));
 
 	return 0;
 }
