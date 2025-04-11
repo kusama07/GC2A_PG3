362f9bb895d0a19b3886388d5f5c3df61c8e25d0
kusama07 <115055012+kusama07@users.noreply.github.com>
2025-04-11 15:39:31 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
-// 関数テンプレート
-template <typename Type>
-Type Min(Type a, Type b) {
-	return a < b ? a : b;
+int General(int time) {
+
+	return time * 1072;
 }
 
-// テンプレート解除
-template <>
-char Min<char>(char a, char b) {
-	return printf("数字以外は代入できません");
+int Recursive(int time,int salary = 100) {
+	if (time <= 1) {
+		return salary;
+	}
+
+	return (salary + Recursive(time - 1 , salary * 2 - 50));
 }
 
 int main() {
 
-	// int
-	printf("%d\n", Min<int>(1, 2));
-	// float
-	printf("%.1f\n", Min<float>(2.5f, 1.5f));
-	// double
-	printf("%lf\n", Min<double>(7.55425, 5.23625));
-	// char
-	printf("%c\n", Min<char>('a', 'b'));
+	// 一般的な方法
+	printf("%d\n", General(8));
+
+	// 再帰的な方法
+	printf("%d\n", Recursive(8));
 
 	return 0;
 }
