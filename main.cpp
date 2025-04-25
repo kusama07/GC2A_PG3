8dceb37088c99bc46e02270b3655905221c0ac00
kusama07 <115055012+kusama07@users.noreply.github.com>
2025-04-25 13:35:48 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -2,63 +2,69 @@
 #include <time.h>
 #include <stdlib.h>
 #include <windows.h>
-
-typedef void (*PFunc)(int*);
+#include <functional>
+// コールバック関数の型定義
+typedef std::function<void(int*)> PFunc;
+typedef std::function<void(bool*)> DiceFunc;
 
 // 乱数を生成する関数
-void dice(bool *s) {
-	srand((unsigned)time(nullptr));
-	
-	int diceNum = rand() % 6 + 1;
+void dice(DiceFunc s) {
+   srand((unsigned)time(nullptr));
+   
+   int diceNum = rand() % 6 + 1;
 
-	printf("出目%d\n", diceNum);
+   printf("出目%d\n", diceNum);
 
-	//　答え入力
-	int ans = 0;
-	scanf_s("%d", &ans);
+   // 答え入力
+   int ans = 0;
+   scanf_s("%d", &ans);
 
-	// 答えが正しいかどうかを判定
-	if (ans == 2 && diceNum % 2 == 0 || ans == 1 && diceNum % 2 == 1) {
-		*s = true;
-	} else {
-		*s = false;
-	}
+   bool isCorrect;
+
+   // 答えが正しいかどうかを判定
+   if (ans == 2 && diceNum % 2 == 0 || ans == 1 && diceNum % 2 == 1) {
+       isCorrect = true;
+   } else {
+       isCorrect = false;
+   }
+
+   // コールバック関数を呼び出す
+   s(&isCorrect);
 }
 
 // コールバック関数
-void correctOutPut(int *s) {
-	printf("正解\n");
+void correctOutPut(bool* s) {
+   printf("正解\n");
 }
 
 // コールバック関数
-void incorrectOutput(int *s) {
-	printf("不正解\n");
+void incorrectOutput(bool* s) {
+   printf("不正解\n");
 }
 
 // タイムアウトを設定する関数
 void SetTimeout(PFunc p, int second) {
-	Sleep(second * 1000);
+   Sleep(second * 1000);
 
-	p(&second);
+   p(&second);
 };
 
 int main() {
-	PFunc correct;
-	correct = correctOutPut;
-	PFunc incoreect;
-	incoreect = incorrectOutput;
-	bool isCorrect;
+   // コールバック関数のポインタを設定
+   DiceFunc correct = [](bool* s) { correctOutPut(s); };
+   DiceFunc incorrect = [](bool* s) { incorrectOutput(s); };
+   
+   printf("半（奇数）の場合 1 , 丁（偶数）の場合 2 を入力 : ");
+   // 乱数を生成して、正解か不正解かを判定
+   DiceFunc diceResult = [correct, incorrect](bool* isCorrect) {
+       if (*isCorrect) {
+           correct(isCorrect);
+       } else {
+           incorrect(isCorrect);
+       }
+   };
 
-	printf("半（奇数）の場合 1 , 丁（偶数）の場合 2 を入力 : ");
-	// 乱数を生成して、正解か不正解かを判定
-	dice(&isCorrect);
+   dice(diceResult);
 
-	// 正解か不正解かを判定
-	if (isCorrect) {
-		SetTimeout(correct, 3);
-	} else {
-		SetTimeout(incoreect, 3);
-	}
-
-	return 0;
+   return 0;
 }
