a717dd76bb24c65e281b59950d151edb23cc8f83
kusama07 <115055012+kusama07@users.noreply.github.com>
2025-05-16 16:56:04 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <iostream>
+
+using namespace std;
 
 int main() {
+	
+	vector<string> idList = {
+		"k022@0032",
+		"k022@0038",
+		"k022@0002",
+		"k022@0029",
+		"k022@0100",
+		"k022@0012",
+	};
 
-	char str[] = "あいえお";
-	printf("%s", str);
+	// 昇順に並べ替え
+	sort(idList.begin(),idList.end());
+
+	// 昇順に並べ替えた結果を表示
+	for (auto itr = idList.begin(); itr != idList.end(); ++itr) {
+		cout << *itr << "\n";
+	}
 
 	return 0;
 }
