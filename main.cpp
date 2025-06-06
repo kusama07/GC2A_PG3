cc83ebba746595ffb7ca688c4abf48711278613d
kusama07 <115055012+kusama07@users.noreply.github.com>
2025-06-06 16:10:30 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <thread>
+
+// 文字出力関数
+void PrintThread1() {
+	printf("Thread1\n");
+}
+
+void PrintThread2() {
+	printf("Thread2\n");
+}
+
+void PrintThread3() {
+	printf("Thread3\n");
+}
 
 int main() {
+	std::thread th1(PrintThread1);
+	// スレッドを開始
+	th1.join(); 
 
+	std::thread th2(PrintThread2);
+	// スレッドを開始
+	th2.join(); 
+
+	std::thread th3(PrintThread3);
+	// スレッドを開始
+	th3.join();
 
 	return 0;
 }
