07a411dc7ec3bd66a30d36d5b15f26d3e507891b
kusama07 <115055012+kusama07@users.noreply.github.com>
2025-06-20 14:06:00 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -13,10 +13,10 @@ void copy(const std::string& source) {
 }
 
 // 移動関数
-void moveStr(std::string&& source) {
+void moveStr(const std::string&& source) {
 	std::string destination;
 	std::chrono::microseconds start = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch());
-	destination = std::move(source); // 文字列のムーブ
+	destination = source; // 文字列のムーブ
 	std::chrono::microseconds end = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch());
 	std::chrono::microseconds duration = end - start;
 	printf("移動: %lld μs\n", duration.count());
@@ -26,7 +26,7 @@ int main() {
 	std::string a(100000, 'a');
 
 	copy(a); // コピー
-	moveStr(); // ムーブ
+	moveStr(std::move(a)); // ムーブ
 
 	return 0;
 }
